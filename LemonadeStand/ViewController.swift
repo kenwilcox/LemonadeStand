0f8f26266e91d4800038bd4c537842a260db4bd4
kenwilcox <wilcoxk@gmail.com>
2015-01-15 00:00:03 -0700

    Outlets and actions

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var balanceLabel: UILabel!
+  @IBOutlet weak var lemonTotalLabel: UILabel!
+  @IBOutlet weak var iceCubeTotalLabel: UILabel!
+  @IBOutlet weak var lemonPurchaseLabel: UILabel!
+  @IBOutlet weak var iceCubePurchaseLabel: UILabel!
+  @IBOutlet weak var lemonMixLabel: UILabel!
+  @IBOutlet weak var iceCubeMixLabel: UILabel!
+  
+  @IBOutlet weak var lemonPurchaseStepper: UIStepper!
+  @IBOutlet weak var iceCubePurchaseStepper: UIStepper!
+  @IBOutlet weak var lemonMixStepper: UIStepper!
+  @IBOutlet weak var iceCubeMixStepper: UIStepper!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func lemonPurchaseValueChanged(sender: UIStepper) {
+  }
+  @IBAction func iceCubePurchaseValueChanged(sender: UIStepper) {
+  }
+  @IBAction func lemonMixValueChanged(sender: UIStepper) {
+  }
+  @IBAction func iceCubeMixValueChange(sender: UIStepper) {
   }
 
   override func didReceiveMemoryWarning() {
